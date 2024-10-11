335d454c8cde4beca958e31d0967241f0be68c09
shammintithi <tanhashammintithi@gmail.com>
2024-10-12 00:38:01 +0600

    Loop

---
+++ for_loop.py
@@ -0,0 +1,68 @@
+#1. for list values
+"""list = [1, 2, 3, 4, 5]
+
+#for name in(keyword) list
+for element in list :
+    print(element)"""
+
+#2. for touple values
+"""tup = (1, 2, 3, 4, 5, 6)
+for value in tup :
+    print(value)"""
+
+#3. for string
+"""str = "Print characters"
+for char in str :
+    print(char)
+else :
+    print("End")"""
+
+#Practice
+"""tup = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
+x = 25
+
+i = 0
+for value in tup :
+    if(value == x) :
+        print("Found x: " , i)
+    i+=1"""
+    
+#4.range
+"""seq = range(5)
+
+for i in seq :
+    print(i)
+
+#another
+for i in range(2, 7) :
+    print(i)
+
+for i in range(2, 15, 3) : #stat, stop, step
+    print(i)"""
+
+#practice - times table
+"""n = int(input("Enter the number: "))
+
+for i in range(1,11) :
+    print (n * i)"""
+
+#Pass statement
+"""for i in range(5) :
+    pass
+print("Hudai")"""
+
+#factorial
+"""n = 5
+fact = 1
+
+for i in range(1, n + 1) :
+    fact *= i
+    
+print("Factorial: ", fact)"""
+
+n = 5
+sum = 0
+
+for i in range(1, n + 1) :
+    sum += i
+print("Sum: ", sum)
---
+++ while_loop.py
@@ -0,0 +1,78 @@
+#1. iteration
+"""count = 1
+
+while count <= 5 :
+    print("Hello world!")
+    count += 1
+
+    print(count)
+
+i = 2
+while i < count :
+    print("Iteration",i)
+    i+=1
+    print(i)"""
+
+#2. Print index
+"""nums = [1,3,4,56,34,7,24,67]
+fruits = ["apple", "orange", "grape", "avocado", "strawberry"]
+
+idx = 0
+while idx < len(nums):
+    print(nums[idx])
+    idx += 1
+
+index = 0
+while index < len(fruits):
+    print(fruits[index])
+    index += 2"""
+
+#3. find the index of the value
+"""nums = [1, 3, 4, 56, 34, 7, 24, 67]
+
+x = 56
+
+i = 0
+while i < len(nums) :
+    if(nums[i] == x):
+        print("Found the index of x: ",i)
+        break
+    else :
+        print("Not found it")
+    i += 1"""
+
+#4. break
+"""i = 0
+while i <= 8:
+    print(i)
+    if(i == 4):
+        break
+    i += 1
+print("End of loop")"""
+
+#5. continue
+"""i = 0
+while i <= 8:
+    if(i == 4):
+        i +=1
+        continue
+    print(i)
+    i += 1"""
+
+#For odd number
+"""i = 0
+while i <= 8:
+    if(i % 2 == 0):
+        i +=1
+        continue
+    print(i)
+    i += 1"""
+
+#For even number
+i = 0
+while i <= 8:
+    if(i % 2 != 0):
+        i +=1
+        continue
+    print(i)
+    i += 1
